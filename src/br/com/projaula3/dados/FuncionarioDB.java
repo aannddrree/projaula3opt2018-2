ee6414411a195cd929b54500345f041c754825cc
Professor Andre Silva <andre@fiar.local>
2018-08-21 20:30:17 -0300

    Execicio em Sala - Aula 3

---
+++ src/br/com/projaula3/dados/FuncionarioDB.java
@@ -0,0 +1,10 @@
+package br.com.projaula3.dados;
+
+import br.com.projaula3.entidade.Funcionario;
+
+public class FuncionarioDB {
+	
+	public boolean insert(Funcionario funcionario) {
+		return false;
+	}
+}
